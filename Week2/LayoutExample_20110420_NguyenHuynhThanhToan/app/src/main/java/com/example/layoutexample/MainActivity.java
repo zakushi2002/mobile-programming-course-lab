22073a149a310f5461475f92587e5726785aa024
ToanNguyen <toannguyenit239@gmail.com>
2023-02-10 11:05:57 +0700

    [Lab1] Layouts Part 2

--- Week2/LayoutExample_20110420_NguyenHuynhThanhToan/app/src/main/java/com/example/layoutexample/MainActivity.java
+++ Week2/LayoutExample_20110420_NguyenHuynhThanhToan/app/src/main/java/com/example/layoutexample/MainActivity.java
@@ -9,6 +9,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.linear_layout);
+        // setContentView(R.layout.linear_layout);
+        setContentView(R.layout.relative_layout);
     }
 }
