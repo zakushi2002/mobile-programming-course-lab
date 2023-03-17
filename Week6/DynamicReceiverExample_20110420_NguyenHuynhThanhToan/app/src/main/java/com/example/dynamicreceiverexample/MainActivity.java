ddebda8e0ba2fbfc197b4c054dd72a7438add64c
ToanNguyen <toannguyenit239@gmail.com>
2023-03-17 10:48:08 +0700

    Broadcast Receiver - Week6 - Part 2
    Practice 2: Dynamic Receiver

---
+++ Week6/DynamicReceiverExample_20110420_NguyenHuynhThanhToan/app/src/main/java/com/example/dynamicreceiverexample/MainActivity.java
@@ -0,0 +1,30 @@
+package com.example.dynamicreceiverexample;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.IntentFilter;
+import android.net.ConnectivityManager;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+    ExampleBroadcastReceiver example = new ExampleBroadcastReceiver();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        IntentFilter filter = new IntentFilter(ConnectivityManager.CONNECTIVITY_ACTION);
+        registerReceiver(example, filter);
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        unregisterReceiver(example);
+    }
+}
